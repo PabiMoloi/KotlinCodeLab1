f0e9ae2385baadb27ac0275ad5fdaedeb3e848d4
Paballo Moloi <PMoloi@jhb.dvt.co.za>
2018-02-27 07:19:22 +0200

    Project upload

---
+++ app/src/main/java/za/co/projects/android/pmoloi/kotlincodelab/MainActivity.kt
@@ -0,0 +1,12 @@
+package za.co.projects.android.pmoloi.kotlincodelab
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
